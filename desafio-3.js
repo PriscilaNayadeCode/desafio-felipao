9e4490a77bc3cf45c0c09a0e6e4c5de3af5f4f23
PriNayadeCode <priscilanayade@gmail.com>
2024-03-29 13:41:09 -0300

    projeto finalizado

--- desafio-3.js
+++ desafio-3.js
@@ -4,4 +4,26 @@ class Heroi{
         this.idade = idade;
         this.tipo = tipo;   
     }
+
+    atacar(){
+        let ataque;
+
+        if(this.tipo === "mago"){
+            ataque = "magia";
+        } else if(this.tipo === "guerreiro"){
+            ataque = "espada"
+        } else if(this.tipo === "monge"){
+            ataque = "artes marciais"
+        } else if(this.tipo === "ninja"){
+            ataque = "shuriken"
+        } else {
+            ataque = "forma desconhecida";
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    
+    }
 }
+
+let meuHeroi = new Heroi ("Gandalf", 2000, "mago")
+meuHeroi.atacar()
